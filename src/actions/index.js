bb48136722ca1eace058f276875a6b0da8c1eabf
alpenfritz <mymailismissing@gmail.com>
2018-04-04 20:25:35 +0900

    ajax request fetchWeather action creator using axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
 const keys = require('../../config/keys');
+
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${keys.openWeatherAPI}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city}`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  };
+}
